9aebaa1c2a9bf01227b164e7059dcd3c5c81aaa3
vcedgar <vatriciaedgar@gmail.com>
2016-11-26 12:24:08 -0700

    Update check.h

--- check.h
+++ check.h
@@ -2,7 +2,7 @@
 
 class Check {
 public:
-	bool check(bool aColor, bool dColor);
+	bool check(bool color);//takes in the color of the defending player and returns true if their king is in check
 private:
 	Bitboard getAllMoves(bool color);//returns a bitboard with all the possible attacking moves
 	Bitboard getKingPos(bool color);//returns a bitboard with the defending king's position
