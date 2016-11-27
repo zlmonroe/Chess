41a7d0209c950b93830a3e4beeedd14e360a8769
vcedgar <vatriciaedgar@gmail.com>
2016-11-27 15:45:50 -0700

    getAllMoves fnc added

--- MoveGenerator.cpp
+++ MoveGenerator.cpp
@@ -38,3 +38,35 @@ Bitboard MoveGenerator::getQueenMoves(uint8_t position, bool isBlack) {
 Bitboard MoveGenerator::getRookMoves(uint8_t position, bool isBlack) {
     return 0;
 }
+Bitboard MoveGenerator::getAllMoves(bool color) {
+	Bitboard totalMoves = ChessConstBitboards::BLANK;
+	int piece;
+	Bitboard location;
+	for (int i = 0; i < 64; i++) {
+		location = 1 << i;
+		piece = ChessBoard::findBoard(color, location);
+		if (piece> 0) {
+			switch (piece) {
+			case 0:
+				totalMoves = totalMoves | MoveGenerator::getPawnMoves(location, color);
+				break;
+			case 1:
+				totalMoves = totalMoves | MoveGenerator::getRookMoves(location, color);
+				break;
+			case 2:
+				totalMoves = totalMoves | MoveGenerator::getKnightMoves(location, color);
+				break;
+			case 3:
+				totalMoves = totalMoves | MoveGenerator::getBishopMoves(location, color);
+				break;
+			case 4:
+				totalMoves = totalMoves | MoveGenerator::getQueenMoves(location, color);
+				break;
+			case 5:
+				totalMoves = totalMoves | MoveGenerator::getKingMoves(location, color);
+				break; //not necessary
+			}
+		}
+	}
+	return totalMoves;
+}
