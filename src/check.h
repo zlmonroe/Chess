d2bbf3cc7a7d5e0e90a745fb79e6143b3ae0c0c5
vcedgar <vatriciaedgar@gmail.com>
2016-11-30 12:55:30 -0700

    Update check.h

--- src/check.h
+++ src/check.h
@@ -2,10 +2,9 @@
 
 class Check {
 public:
+    Check(MoveGenerater m);
     bool check(bool color);//takes in the color of the defending player and returns true if their king is in check
-    -private:
-    -	Bitboard getAllMoves(bool color);//returns a bitboard with all the possible attacking moves
-    -	Bitboard getKingPos(bool color);//returns a bitboard with the defending king's position
-    -
-    +private:
-};
+    private:
+        MoveGenerator moves;
+    	Bitboard getAllMoves(bool color);//returns a bitboard with all the possible attacking moves    -
+   };
