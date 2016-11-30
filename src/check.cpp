0ab0c650b3185d43b8ef4d5d9830c26c9758d7f4
vcedgar <vatriciaedgar@gmail.com>
2016-11-30 12:55:24 -0700

    Update check.cpp

--- src/check.cpp
+++ src/check.cpp
@@ -2,35 +2,38 @@
  #include "ChessConstBitboards.h"
  #include "MoveGenerator.h"
  #include "ChessBoard.h"
- 
+
+Check::Check(MoveGenerator m){
+  moves = m;
+}
  Bitboard Check::getAllMoves(bool color) {
  	Bitboard totalMoves = BLANK;
  	int piece;
  	Bitboard location;
  	for (int i = 0; i < 64; i++) {
  		location = 1 << i;
- 		piece = ChessBoard::findBoard(color, location);
+ 		piece = moves.chessBoard[0].findBoard(color, location);
  		if (piece> 0) {
  			switch (piece) {
  			case 0:
- 				totalMoves = totalMoves | MoveGenerator::getPawnMoves(location, color);
+ 				totalMoves = totalMoves | moves.getPawnMoves(location, color);
  				break;
  			case 1:
- 				totalMoves = totalMoves | MoveGenerator::getRookMoves(location, color);
+ 				totalMoves = totalMoves | moves.getRookMoves(location, color);
  				break;
  			case 2:
- 				totalMoves = totalMoves | MoveGenerator::getKnightMoves(location, color);
+ 				totalMoves = totalMoves | moves.getKnightMoves(location, color);
  				break;
  			case 3:
- 				totalMoves = totalMoves | MoveGenerator::getBishopMoves(location, color);
+ 				totalMoves = totalMoves |moves.getBishopMoves(location, color);
  				break;
  			case 4:
- 				totalMoves = totalMoves | MoveGenerator::getQueenMoves(location, color);
+ 				totalMoves = totalMoves | moves.getQueenMoves(location, color);
   				break;
   			case 5:
-  				totalMoves = totalMoves | MoveGenerator::getKingMoves(location, color);
- -				break;
- +				break; //not necessary
+  				totalMoves = totalMoves | moves.getKingMoves(location, color);
+ 				break;
+ 				break; //not necessary
   			}
   		}
   	}
