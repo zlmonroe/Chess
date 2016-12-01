2965f6fec5cb2b15fe8774f775bd4d1799fcb87b
vcedgar <vatriciaedgar@gmail.com>
2016-11-30 18:04:31 -0700

    Update check.cpp

--- src/check.cpp
+++ src/check.cpp
@@ -6,39 +6,6 @@
 Check::Check(MoveGenerator m){
   moves = m;
 }
- Bitboard Check::getAllMoves(bool color) {
- 	Bitboard totalMoves = BLANK;
- 	int piece;
- 	Bitboard location;
- 	for (int i = 0; i < 64; i++) {
- 		location = 1 << i;
- 		piece = moves.chessBoard[0].findBoard(color, location);
- 		if (piece> 0) {
- 			switch (piece) {
- 			case 0:
- 				totalMoves = totalMoves | moves.getPawnMoves(location, color);
- 				break;
- 			case 1:
- 				totalMoves = totalMoves | moves.getRookMoves(location, color);
- 				break;
- 			case 2:
- 				totalMoves = totalMoves | moves.getKnightMoves(location, color);
- 				break;
- 			case 3:
- 				totalMoves = totalMoves |moves.getBishopMoves(location, color);
- 				break;
- 			case 4:
- 				totalMoves = totalMoves | moves.getQueenMoves(location, color);
-  				break;
-  			case 5:
-  				totalMoves = totalMoves | moves.getKingMoves(location, color);
- 				break;
- 				break; //not necessary
-  			}
-  		}
-  	}
- 	return totalMoves;
- }
  Bitboard Check::check(bool color) {
- 	return ((bool)(ChessBoard::pieces[color][5] & getAllMoves(!color)));
+ 	return ((bool)(ChessBoard::pieces[color][5] & moves.getAllMoves(!color)));
  }
