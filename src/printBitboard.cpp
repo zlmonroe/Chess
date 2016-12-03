1b56388018eaaa5837a446c300e8bbf0b17c525c
zlmonroe <zachary.lynn.monroe@gmail.com>
2016-12-03 16:51:31 -0700

    Prettier Chessboard!

--- src/printBitboard.cpp
+++ src/printBitboard.cpp
@@ -1,4 +1,6 @@
 #include "printBitboard.h"
+#include <iostream>
+#include <windows.h>
 
 void printBitboard(const Bitboard bitboard) {
     for (int row = 7; row >= 0; --row) {
@@ -16,33 +18,53 @@ void printBitboard(const Bitboard bitboard) {
 }
 
 void  printChessBoard(ChessBoard chessBoard) {
+	HANDLE hstdin  = GetStdHandle( STD_INPUT_HANDLE  );
+	HANDLE hstdout = GetStdHandle( STD_OUTPUT_HANDLE );
+
+	// Remember how things were when we started
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo( hstdout, &csbi );
+
+    WORD colors[] = {0x0F, 0xF0};
+
     std::cout << "Current Board:\n";
     std::string board[64];
     for (int i=0; i<64; i++) {
-        if      ((chessBoard.pieces[0][0] >> i) & 0b1) board[i]="P";
-        else if ((chessBoard.pieces[0][1] >> i) & 0b1) board[i]="R";
-        else if ((chessBoard.pieces[0][2] >> i) & 0b1) board[i]="N";
-        else if ((chessBoard.pieces[0][3] >> i) & 0b1) board[i]="B";
-        else if ((chessBoard.pieces[0][4] >> i) & 0b1) board[i]="Q";
-        else if ((chessBoard.pieces[0][5] >> i) & 0b1) board[i]="K";
+        if      ((chessBoard.pieces[0][0] >> i) & 0b1) board[i]=" P ";
+        else if ((chessBoard.pieces[0][1] >> i) & 0b1) board[i]=" R ";
+        else if ((chessBoard.pieces[0][2] >> i) & 0b1) board[i]=" N ";
+        else if ((chessBoard.pieces[0][3] >> i) & 0b1) board[i]=" B ";
+        else if ((chessBoard.pieces[0][4] >> i) & 0b1) board[i]=" Q ";
+        else if ((chessBoard.pieces[0][5] >> i) & 0b1) board[i]=" K ";
 
-        else if ((chessBoard.pieces[1][0] >> i) & 0b1) board[i]="p";
-        else if ((chessBoard.pieces[1][1] >> i) & 0b1) board[i]="r";
-        else if ((chessBoard.pieces[1][2] >> i) & 0b1) board[i]="n";
-        else if ((chessBoard.pieces[1][3] >> i) & 0b1) board[i]="b";
-        else if ((chessBoard.pieces[1][4] >> i) & 0b1) board[i]="q";
-        else if ((chessBoard.pieces[1][5] >> i) & 0b1) board[i]="k";
-        else board[i]= (char)254;
+        else if ((chessBoard.pieces[1][0] >> i) & 0b1) board[i]=" p ";
+        else if ((chessBoard.pieces[1][1] >> i) & 0b1) board[i]=" r ";
+        else if ((chessBoard.pieces[1][2] >> i) & 0b1) board[i]=" n ";
+        else if ((chessBoard.pieces[1][3] >> i) & 0b1) board[i]=" b ";
+        else if ((chessBoard.pieces[1][4] >> i) & 0b1) board[i]=" q ";
+        else if ((chessBoard.pieces[1][5] >> i) & 0b1) board[i]=" k ";
+        else board[i]="   ";
     }
-    std::cout << "    A B C D E F G H\n    ---------------\n8  |";
+    SetConsoleTextAttribute( hstdout, 0x6F);
+    std::cout << "\n|  A  B  C  D  E  F  G  H  |\n|8";
     for (int row = 7; row>=0; row--) {
         for (int col = 0; col<8; col++) {
             short index = row*8+col;
+            SetConsoleTextAttribute( hstdout, colors[(index+row%2)%2] );
             std::cout << board[index];
-            if ((index+1)%8==0 && index+1!=8)
-                std::cout << "|  " << row+1 <<" \n" << row << "  |";
-            else if (index+1!=8) std::cout << " ";
+            SetConsoleTextAttribute( hstdout, csbi.wAttributes );
+            if ((index+1)%8==0 && index+1!=8) {
+                SetConsoleTextAttribute( hstdout, 0x6F);
+                std::cout << row+1 <<"|\n" << "|" << row;
+            }
+            else if (index+1!=8) std::cout << "";
         }
     }
-    std::cout << "|  1\n    ---------------\n    A B C D E F G H\n";
+    SetConsoleTextAttribute( hstdout, 0x6F);
+    std::cout << "1|\n|  A  B  C  D  E  F  G  H  |\n";
+
+    FlushConsoleInputBuffer( hstdin );
+
+	// Keep users happy
+	SetConsoleTextAttribute( hstdout, csbi.wAttributes );
 }
